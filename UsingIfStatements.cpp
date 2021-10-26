1adebdee1148145d2f2a622b47c21f483a604711
Pchemelil <85558913+Pchemelil@users.noreply.github.com>
2021-10-26 20:45:23 +0300

    Add files via upload

---
+++ UsingIfStatements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	cout<<"Please enter their age:\n";
+	cin>> age;
+	
+	if (age>=4){
+		cout<< "admit child to school.";
+		
+	} else 
+		cout<<"do not admit child";
+	
+	return 0;
+}
